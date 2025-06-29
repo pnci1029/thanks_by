d9a2b717fa598e08243494b012bec7c344f58190
chhong <pnci1029@gmail.com>
2025-06-29 19:23:27 +0900

    이모ㅓ티콘 테마 변경

--- lib/widgets/emotion_tag_selector.dart
+++ lib/widgets/emotion_tag_selector.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class EmotionTagSelector extends StatelessWidget {
+class EmotionTagSelector extends StatefulWidget {
   final List<String> tags;
   final String selectedTag;
   final ValueChanged<String> onChanged;
@@ -13,37 +13,84 @@ class EmotionTagSelector extends StatelessWidget {
   });
 
   @override
+  State<EmotionTagSelector> createState() => _EmotionTagSelectorState();
+}
+
+class _EmotionTagSelectorState extends State<EmotionTagSelector>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onTagSelected(String tag) {
+    _controller.forward().then((_) => _controller.reverse());
+    widget.onChanged(tag);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Wrap(
       spacing: 12,
       runSpacing: 12,
-      children: tags.map((tag) {
-        final isSelected = tag == selectedTag;
+      children: widget.tags.map((tag) {
+        final isSelected = tag == widget.selectedTag;
         return GestureDetector(
-          onTap: () => onChanged(tag),
-          child: Container(
+          onTap: () => _onTagSelected(tag),
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
             decoration: BoxDecoration(
-              color: isSelected ? const Color(0xFF4CAF50) : Colors.white,
+              color: isSelected ? theme.primaryColor : Colors.white,
               borderRadius: BorderRadius.circular(25),
               border: Border.all(
-                color: isSelected ? const Color(0xFF4CAF50) : Colors.grey.shade300,
+                color: isSelected ? theme.primaryColor : Colors.grey.shade300,
                 width: 2,
               ),
-              boxShadow: isSelected ? [
-                BoxShadow(
-                  color: const Color(0xFF4CAF50).withOpacity(0.3),
-                  blurRadius: 8,
-                  offset: const Offset(0, 2),
-                ),
-              ] : null,
+              boxShadow: isSelected
+                  ? [
+                      BoxShadow(
+                        color: theme.primaryColor.withOpacity(0.3),
+                        blurRadius: 8,
+                        offset: const Offset(0, 2),
+                      ),
+                    ]
+                  : [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.1),
+                        blurRadius: 4,
+                        offset: const Offset(0, 1),
+                      ),
+                    ],
             ),
-            child: Text(
-              tag,
-              style: TextStyle(
-                fontSize: 18,
-                color: isSelected ? Colors.white : Colors.black87,
-                fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+            child: AnimatedScale(
+              scale: isSelected ? 1.1 : 1.0,
+              duration: const Duration(milliseconds: 200),
+              child: Text(
+                tag,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: isSelected ? Colors.white : Colors.black87,
+                  fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+                ),
               ),
             ),
           ),
